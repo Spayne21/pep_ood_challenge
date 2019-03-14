831ea33a90f4bdec3daf894ec8548d6931d39d7c
Spayne21 <s.dpayne@outlook.com>
2019-03-14 21:46:20 +0000

    commit

--- player_selector.rb
+++ player_selector.rb
@@ -1,22 +1,50 @@
 ####### Program Dependencies ########
 require 'json'
 # Templates - Remember remove or edit these line if you edit the file names etc.
-require './classes/class_template'
 require './modules/module_template'
 # Must explicitly include modules to use them, unlike classes which are instantiated (created).
 include ModuleTemplate
 
-####### My Solution ########
-# Load and assign data to a variable here.
-transfer_targets = File.read('transfer_targets.json')
-transfer_targets = JSON.parse(transfer_targets)
-# Tip: Inspect the data first with prettify (shift + command + p). The data is a bunch of hash objects within a single array (this is how most json data will be displayed/formatted).
+targets = File.read('transfer_targets.json')
+targets = JSON.parse(targets)
 
-# Example Class.
-ClassTemplate.new('Hello Class World').call
+clubs_list = [
+  'Arsenal', 'Chelsea', 'Leicester City',
+  'Liverpool', 'Manchester United', 'Tottenham Hotspur'
+]
 
-# Example Module ( Tip: Modules can also be included into Classes! ).
-my_module_method('Hello Modular World')
 
+
+def filter_clubs_criteria (targets, clubs_list)
+  targets.delete_if { |player| player['Appearances'].to_i < 50 }
+  targets.delete_if { |player| !clubs_list.include?(player['Club']) }
+ 
+end
+def filter_goalkeeper_criteria(targets)
+  targets.delete_if { |player| player['Position'] == 'Goalkeeper' && player["Passes"].to_i < 3000 }
+  targets.delete_if { |player| player['Position'] == 'Goalkeeper' && player["Passes per match"].to_f < 28.00 }
+end
+def filter_defender_criteria(targets)
+  targets.delete_if { |player| player['Position'] == 'Defender' && player['Tackle success %'].to_i < 81 }
+  targets.delete_if { |player| player['Position'] == 'Defender' && player['Red cards'].to_i > 4 }
+end
+
+def filter_midfielder_criteria(targets) 
+targets.delete_if { |player| player['Position'] == 'Midfielder' && player['Goals'].to_i + player['Assists'].to_i < 80 }
+end
+
+
+def filter_forward_criteria(targets)
+targets.delete_if { |player| player['Position'] == 'Forward' && player['Shots'].to_i <= 99 }
+targets.delete_if { |player| player['Position'] == 'Forward' && player['Shots on target'].to_i * 100 / player['Shots'].to_i <= 44 }
+end
+
+
+
+filter_clubs_criteria(targets, clubs_list)
+filter_goalkeeper_criteria(targets)
+filter_defender_criteria(targets)
+filter_midfielder_criteria(targets)
+filter_forward_criteria(targets)
 # # Then 'write' your sorted transfer targets into the successful_candidates.json file.
-File.write('successful_candidates.json', transfer_targets.to_json)
+File.write('successful_candidates.json', targets.to_json)
